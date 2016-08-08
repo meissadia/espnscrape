e1e71a4c3650ce447d92948472acf6a0729f0de7
Meissa Dia <meissadia@gmail.com>
2016-08-08 11:42:40 -0600

    DISCONTINUED

--- lib/espnscrape/Hash.rb
+++
@@ -1,9 +0,0 @@
-# Extend Hash
-class Hash
-  # Get an NbaBoxscore
-  # @param [Symbol] Format
-  # @return [NbaBoxscore] Boxscore
-  def boxscore(f_mat = nil)
-    EspnScrape.boxscore(self[:boxscore_id], (f_mat || :to_hashes))
-  end
-end
